8e4a72cce0b98df4b1776e701bdd0f3466995108
anasta47 <iakimova.work@gmail.com>
2024-09-18 10:07:40 +0700

    Update Rolling retention.sql

--- Rolling retention.sql
+++ Rolling retention.sql
@@ -11,20 +11,20 @@ WITH tab as(
     where to_char(date_joined, 'YYYY') = '2022'
   )
 SELECT cohort,
-    count(DISTINCT CASE when diff>=0 then user_id end)*100.0/ count(DISTINCT case when diff>=0 then user_id end) as "0 day",
+    count(DISTINCT CASE when diff>=0 then user_id end)*100.0/ count(DISTINCT case when diff>=0 then user_id end) as "Day 0",
     round(count(DISTINCT CASE when diff>=1 then user_id end)*100.0/ 
-count (DISTINCT CASE when diff>=0 then user_id end), 2) as "1 day",
+count (DISTINCT CASE when diff>=0 then user_id end), 2) as " Day 1",
     round(count(DISTINCT CASE when diff>=3 then user_id end)*100.0/ 
-count (DISTINCT CASE when diff>=0 then user_id end), 2) as "3 day",
+count (DISTINCT CASE when diff>=0 then user_id end), 2) as "Day 3",
     round(count(DISTINCT CASE when diff>=7 then user_id end)*100.0/ 
-count (DISTINCT CASE when diff>=0 then user_id end), 2) as "7 day",
+count (DISTINCT CASE when diff>=0 then user_id end), 2) as "Day 7",
     round(count(DISTINCT CASE when diff>=14 then user_id end)*100.0/ 
-count (DISTINCT CASE when diff>=0 then user_id end), 2) as "14 day",
+count (DISTINCT CASE when diff>=0 then user_id end), 2) as "Day 14",
     round(count(DISTINCT CASE when diff>=30 then user_id end)*100.0/ 
-count (DISTINCT CASE when diff>=0 then user_id end), 2) as "30 day",
+count (DISTINCT CASE when diff>=0 then user_id end), 2) as "Day 30",
     round(count(DISTINCT CASE when diff>=60 then user_id end)*100.0/ 
-count (DISTINCT CASE when diff>=0 then user_id end), 2) as "60 day",
+count (DISTINCT CASE when diff>=0 then user_id end), 2) as "Day 60",
     round(count(DISTINCT CASE when diff>=90 then user_id end)*100.0/ 
-count (DISTINCT CASE when diff>=0 then user_id end), 2) as "90 day"
+count (DISTINCT CASE when diff>=0 then user_id end), 2) as "Day 90"
 FROM tab t
 group by cohort
